6bd47e95046154cb104683fd7e4b6750b51f050f
iamohmx <48460454+iamohmx@users.noreply.github.com>
2024-09-26 16:51:36 +0700

    ddd

--- src/main/java/org/sep26/Main.java
+++ src/main/java/org/sep26/Main.java
@@ -1,11 +1,6 @@
 package org.sep26;//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 
-import org.sep26.config.DBConnect;
-import org.sep26.controllers.CarController;
 import org.sep26.controllers.LogicController;
-import org.sep26.models.Car;
-
-import java.sql.SQLException;
 import java.util.Scanner;
 
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
@@ -22,6 +17,62 @@ public class Main {
         LogicController logicController = new LogicController();
         logicController.insertCar(plate, model, make);
     }
+
+    public static void updateCar() {
+
+    }
+
+    public static void searchCar() {
+
+    }
+
+    public static void deleteCar() {
+
+    }
+
+    public static void viewCars() {
+
+    }
+
+    public void mainMenu() {
+        Scanner scanner = new Scanner(System.in);
+        boolean cont = true;
+        while (cont) {
+            System.out.println("=== Main Menu ===");
+            System.out.println("1 Add Car.");
+            System.out.println("2 Update Car.");
+            System.out.println("3 Search Car.");
+            System.out.println("4 Delete Car.");
+            System.out.println("9 View Cars.");
+            System.out.println("0 Exit");
+            System.out.print("Your choice:");
+            String choice = scanner.nextLine();
+            switch (choice) {
+                case "1":
+                    insertCar();
+                    break;
+                case "2":
+                    updateCar();
+                    break;
+                case "3":
+                    searchCar();
+                    break;
+                case "4":
+                    deleteCar();
+                    break;
+                case "9":
+                    viewCars();
+                    break;
+                case "0":
+                    cont = false;
+                    break;
+                default:
+                    System.out.println("Invalid choice. Please try again.");
+            }
+        }
+    }
+
+
     public static void main(String[] args) {
         insertCar();
     }
